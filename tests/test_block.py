f1528977e5e97535a1c0102dd4c5f098e13e09e4
joeshiett <josepheshiett@gmail.com>
2023-01-13 13:26:14 +0100

    Modify test_block.py

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,4 +1,5 @@
 import pytest 
+import time
 import requests
 from substrateinterface import SubstrateInterface
 from websocket._exceptions import WebSocketBadStatusException
@@ -15,13 +16,13 @@ def test_websocket_rpc():
             assert True, "Websocket Connection established"
 
     except WebSocketBadStatusException as bad_status:
-        pytest.fail(f'failed due to{bad_status}')
+        pytest.fail(f'failed due to {bad_status}')
     except ConnectionRefusedError as conn_refused:
-        pytest.fail(f'failed due to{conn_refused}')
+        pytest.fail(f'failed due to {conn_refused}')
 
 # Test JSON RPC connectivity
 def test_json_rpc():
-    url = 'https://127.0.0.1:9933'
+    url = 'http://127.0.0.1:9933'
     payload = {"jsonrpc":"2.0","method":"system_name","params":[],"id":1}
     response = requests.post(url, json=payload)
     assert response.status_code == 200
@@ -33,14 +34,17 @@ def test_block_production():
             url="ws://127.0.0.1:9944",
         )
         block_header = ws_provider.get_block_header(finalized_only=True)
-
+        
+        # Test if Block number up to 5 in last 60 seconds
         for key, value in block_header['header'].items():
+            time.sleep(60)
             if key == 'number':
-                if value > 0:
-                    assert value > 0
+                if value > 10:
+                    assert value > 10
+                elif value == 0:
+                    pytest.fail(f'block number not exceeding zero!')
+                   
     except WebSocketBadStatusException as bad_status:
-        pytest.fail(f'failed due to{bad_status}')
+        pytest.fail(f'failed due to {bad_status}')
     except ConnectionRefusedError as conn_refused:
-        pytest.fail(f'failed due to{conn_refused}')
-
-    
+        pytest.fail(f'failed due to {conn_refused}')
