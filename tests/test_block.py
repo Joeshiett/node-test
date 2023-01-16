af4611e3da85c6b2fe31a93e2ec959f7a49a3a85
joeshiett <josepheshiett@gmail.com>
2023-01-16 14:28:49 +0100

    Modify test

--- tests/test_block.py
+++ tests/test_block.py
@@ -8,7 +8,7 @@ from websocket._exceptions import WebSocketBadStatusException
 def test_websocket_rpc():
     try:
         ws_provider = SubstrateInterface(
-            url="ws://127.0.0.1:9944",
+            url="wss://wss.api.moonbase.moonbeam.network",
         )
         # Retrieve the finalized block
         ws_conn = ws_provider.connect_websocket()
@@ -24,28 +24,33 @@ def test_websocket_rpc():
 
 # Test JSON RPC connectivity
 def test_json_rpc():
-    url = 'http://127.0.0.1:9933'
+    url = 'http://rpc-0.zeitgeist.pm:9933'
     payload = {"jsonrpc":"2.0","method":"system_name","params":[],"id":1}
     response = requests.post(url, json=payload)
     assert response.status_code == 200
     
 # Test Block Production
 def test_block_production():
-    # Delay function for 60 seconds
-    time.sleep(60)
     try:
         ws_provider = SubstrateInterface(
-            url="ws://127.0.0.1:9944",
+            url="wss://wss.api.moonbase.moonbeam.network",
         )
-        block_header = ws_provider.get_block_header(finalized_only=True)
-        # Test if Block number up to 5 in last 60 seconds
-        for key, value in block_header['header'].items():
-            if key == 'number':
-                if value > 5:
-                    assert value > 5
-                elif value == 0:
-                    pytest.fail(f'block number not exceeding zero!')
-                   
+        
+        # List to store Block numbers
+        block_num = []           
+        def subscription_handler(obj, update_nr, subscription_id):
+            print(f"Block #{obj['header']['number']}")
+            block_num.append(obj['header']['number'])
+            if update_nr > 5:
+                return 'Done' 
+
+        result = ws_provider.subscribe_block_headers(subscription_handler)
+        res = all(i < j for i, j in zip(block_num, block_num[1:]))
+        if res is True:
+            assert True 
+        else:
+            pytest.fail('Blocks not being produced')
+
     except WebSocketBadStatusException as bad_status:
         pytest.fail(f'failed due to {bad_status}')
     except ConnectionRefusedError as conn_refused:
